3ab225d8c181b4110279329d61846a90c8b17ad3
kxmc <mchucan@hotmail.com>
2019-07-17 10:07:17 +0800

    drawPath绘制心形图案

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -21,10 +23,21 @@ public class Practice8DrawArcView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    private Paint paint = new Paint();
+    private RectF rectF = new RectF(150, 150, 350, 300);
+    private RectF rectF2 = new RectF(150, 180, 350, 300);
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawArc(rectF, -180, 50, false, paint);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawArc(rectF, -120, 110, true, paint);
+        canvas.drawArc(rectF2, 30, 120, false, paint);
+
+
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -2,6 +2,11 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -21,10 +26,29 @@ public class Practice9DrawPathView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    private Paint paint = new Paint();
+    private Path path = new Path();
+
+    {
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            path.addArc(100, 100, 150, 150, -225, 225);
+            path.arcTo(150, 100, 200, 150, -180, 225, false);
+            path.lineTo(150, 180);
+        } else {
+            path.addArc(new RectF(100, 100, 150, 150), -225, 225);
+            path.arcTo(new RectF(150, 100, 200, 150), -180, 225, false);
+            path.lineTo(150, 180);
+        }
+
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawPath() 方法画心形
+        canvas.drawPath(path, paint);
     }
 }
