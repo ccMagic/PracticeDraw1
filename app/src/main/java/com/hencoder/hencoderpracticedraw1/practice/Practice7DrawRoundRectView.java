564b18eeab04b46b8189e6196369d8f4d780a09b
kxmc <mchucan@hotmail.com>
2019-07-17 09:32:58 +0800

    圆角矩形

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice7DrawRoundRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice7DrawRoundRectView.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -20,11 +22,16 @@ public class Practice7DrawRoundRectView extends View {
     public Practice7DrawRoundRectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
+    private Paint paint=new Paint();
+    private RectF rectF=new RectF(250,250,550,350);
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawRoundRect() 方法画圆角矩形
+
+        canvas.drawRoundRect(rectF,20,20,paint);
+
     }
 }
